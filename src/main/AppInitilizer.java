3b7e4704b81709f792761de270a531d40a40ebcf
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-12-17 20:48:04 +0530

    implement appInitilizer

--- src/main/AppInitilizer.java
+++ src/main/AppInitilizer.java
@@ -1,4 +1,16 @@
 package main;
 
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class AppInitilizer {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
+        ctx.registerShutdownHook();
+
+
+    }
+
 }
